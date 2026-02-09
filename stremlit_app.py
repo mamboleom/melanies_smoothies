836cc79ab36dbfb920951c2e4d1648fede25f7aa
mamboleom <168001780+mamboleom@users.noreply.github.com>
2026-02-09 15:08:47 +0000

    fixed

--- stremlit_app.py
+++ stremlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         searc_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen,'SEARCH_ON'].iloc[0]
-        st.write('The search value for',fruit_chosen,'is',searc_on,'.')
+        #st.write('The search value for',fruit_chosen,'is',searc_on,'.')
 
         st.subheader(fruit_chosen+'Nutrition Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
