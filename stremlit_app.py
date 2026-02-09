0538af4c807202e2d5273d4976aa00a4a1c27f5b
mamboleom <168001780+mamboleom@users.noreply.github.com>
2026-02-09 15:07:41 +0000

    corrected

--- stremlit_app.py
+++ stremlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
         st.write('The search value for',fruit_chosen,'is',searc_on,'.')
 
         st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # Corrected indentation: this must align with the 'for' loop or the string initialization
     my_insert_stmt = """insert into smoothies.public.orders (ingredients, name_on_order)
