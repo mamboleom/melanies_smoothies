fb4d093c416d8ffc62952eb28db52cf20a491f33
mamboleom <168001780+mamboleom@users.noreply.github.com>
2026-02-09 13:31:58 +0000

    replaced watermelon as default

--- stremlit_app.py
+++ stremlit_app.py
@@ -25,7 +25,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # Corrected indentation: this must align with the 'for' loop or the string initialization
     my_insert_stmt = """insert into smoothies.public.orders (ingredients, name_on_order)
