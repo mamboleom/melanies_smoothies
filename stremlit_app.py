f656c174a3835b95845c0b955bad6c704586f078
mamboleom <168001780+mamboleom@users.noreply.github.com>
2026-02-09 15:05:20 +0000

    making use of searchon

--- stremlit_app.py
+++ stremlit_app.py
@@ -34,9 +34,9 @@ if ingredients_list:
 
         searc_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen,'SEARCH_ON'].iloc[0]
         st.write('The search value for',fruit_chosen,'is',searc_on,'.')
-        
+
         st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # Corrected indentation: this must align with the 'for' loop or the string initialization
     my_insert_stmt = """insert into smoothies.public.orders (ingredients, name_on_order)
